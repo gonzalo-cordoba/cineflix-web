bae68ce8fe311a1d11281d26bf16b198a21175b2
Gonzalo Cordoba <GonzaloCordoba070@gmail.com>
2024-10-30 16:01:46 -0300

    Refactorice la página de inicio para utilizar suspense y separar las secciones de películas con diferentes funciones de obtención de datos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,81 @@
 import CineBanner from "@/components/cine-banner/CineBanner";
-import {
-  MovieResult,
-  MoviesResponse,
-} from "../movies/interfaces/movies.response";
+import { Suspense } from "react";
+
 import { MovieGrid } from "../movies/components/MovieGrid";
 import Link from "next/link";
 import ButtonViolet from "@/components/ui/button-violet/ButtonViolet";
+import {
+  getTopMovies,
+  getUpcomingMovies,
+  getNowPlayingMovies,
+  getPopularMovies,
+} from "../movies/lib/movies";
+import { MovieResult } from "../movies";
 
-const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
-
-const getMovies = async (page = 1, search = ""): Promise<MovieResult[]> => {
-  const data: MoviesResponse = await fetch(
-    `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&page=${page}&language=es-AR`
-  ).then((res) => res.json());
-
-  if (!data.results) {
-    console.error("Error: la respuesta de la api no contiene results");
-    return [];
-  }
-
-  const movies = data.results.map((movie) => ({
-    id: movie.id,
-    title: movie.title,
-    overview: movie.overview,
-    poster_path: movie.poster_path,
-    release_date: movie.release_date,
-    vote_average: movie.vote_average,
-    genre_ids: movie.genre_ids,
-  }));
-
-  return movies;
-};
+async function MovieSection({
+  title,
+  fetchFunction,
+  limit,
+}: {
+  title: string;
+  fetchFunction: () => Promise<MovieResult[]>;
+  limit: number;
+}) {
+  const movies = await fetchFunction();
+  return (
+    <>
+      <h2 className="text-2xl font-bold mb-4">{title}</h2>
+      <MovieGrid movies={movies.slice(0, limit)} />
+    </>
+  );
+}
 
 export default async function Home() {
-  const movies = await getMovies();
-
-  const topMovies = movies.slice(0, 6);
-
   return (
     <>
       <main className="flex min-h-screen flex-col items-center justify-between">
         <CineBanner />
       </main>
-      <main className="flex flex-col justify-center mb-10">
+      <main className="flex flex-col justify-center mb-10 px-4 max-w-7xl mx-auto">
         <h1 className="text-3xl md:text-4xl font-extrabold text-gray-900 dark:text-white mb-6 text-center">
           ¡Descubre el top de películas en tendencia!
         </h1>
 
-        <MovieGrid movies={topMovies} />
+        <Suspense fallback={<div>Cargando estrenos...</div>}>
+          <MovieSection
+            title="Estrenos"
+            fetchFunction={getNowPlayingMovies}
+            limit={6}
+          />
+        </Suspense>
 
-        <Link className="flex justify-center" href={"/movies"}>
+        <Link className="flex justify-center mt-4 mb-8" href={"/movies"}>
           <ButtonViolet text="Descubrir más estrenos" />
         </Link>
+
+        <Suspense fallback={<div>Cargando ventas anticipadas...</div>}>
+          <MovieSection
+            title="Ventas anticipadas"
+            fetchFunction={getUpcomingMovies}
+            limit={4}
+          />
+        </Suspense>
+
+        <Suspense fallback={<div>Cargando películas populares...</div>}>
+          <MovieSection
+            title="Películas populares"
+            fetchFunction={getPopularMovies}
+            limit={3}
+          />
+        </Suspense>
+
+        <Suspense fallback={<div>Cargando películas mejor valoradas...</div>}>
+          <MovieSection
+            title="Películas mejor valoradas"
+            fetchFunction={getTopMovies}
+            limit={3}
+          />
+        </Suspense>
       </main>
     </>
   );
