4a3599a4aee9c8292298f36c282b6d1eba90fc6d
Gonzalo Cordoba <GonzaloCordoba070@gmail.com>
2024-11-26 12:28:23 -0300

    Updated favicon.ico and added icon metadata to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,9 @@ const inter = Montserrat({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Cineflix",
   description: "Las mejores peliculas para vos!",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
