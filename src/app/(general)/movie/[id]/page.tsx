146c87ac02f0f502ec463536fd423027e893abad
Gonzalo Cordoba <GonzaloCordoba070@gmail.com>
2024-12-11 15:20:53 -0300

    `Added getMovie function and updated MoviePage to fetch movie data`

--- src/app/(general)/movie/[id]/page.tsx
+++ src/app/(general)/movie/[id]/page.tsx
@@ -2,21 +2,23 @@ import MovieContent from "../components/MovieContent";
 
 const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
+// Función para obtener la información de la película
 async function getMovie(id: string) {
   const res = await fetch(
     `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=es-AR&region=AR`,
-    { next: { revalidate: 3600 } }
+    { next: { revalidate: 3600 } } // Revalidación cada hora
   );
   if (!res.ok) throw new Error("Failed to fetch movie");
   return res.json();
 }
 
+// Página principal
 export default async function MoviePage({
   params,
 }: {
   params: { id: string };
 }) {
-  const movie = await getMovie(params.id);
+  const movie = await getMovie(params.id); // Obtén los datos de la película
   const backgroundUrl = `https://image.tmdb.org/t/p/original${movie.poster_path}`;
 
   return (
